9587edd7555fa1d9856fd7e068ce693f73ecca5e
huy023156 <huy023156@gmail.com>
2025-07-05 14:48:32 +0700

    Create Channel

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -6,6 +6,7 @@ import { CreateServerModal } from "@/components/modals/create-server-modal";
 import { InviteModal } from "@/components/modals/invite-modal";
 import { EditServerModal } from "../modals/edit-server-modal";
 import { MembersModal } from "../modals/members-modal";
+import { CreateChannelModal } from "../modals/create-channel-modal";
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
 
@@ -13,7 +14,7 @@ export const ModalProvider = () => {
 		setIsMounted(true);
 	}, []);
 
-	if (!isMounted) {
+  if (!isMounted) {
 		return null;
 	}
 
@@ -23,6 +24,7 @@ export const ModalProvider = () => {
       <InviteModal />
       <EditServerModal />
       <MembersModal />
+      <CreateChannelModal />
     </>
   )
 }
